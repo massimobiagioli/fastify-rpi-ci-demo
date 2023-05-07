4ff53c05f80765b5ecf0ea948950c549edd94057
massimobiagioli <biagiolimassimo@gmail.com>
2023-05-07 09:11:12 +0200

    Fix server

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ app.register(import('./app.js'))
 
 const start = async function () {
   try {
-    await app.listen({ port: process.env.PORT || 3000 })
+    await app.listen(
+        { 
+            port: process.env.PORT || 3000,
+            host: '0.0.0.0' 
+        }
+    )
   } catch (e) {
     console.error(e)
   }
